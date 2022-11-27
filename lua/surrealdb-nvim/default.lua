3eb1705e15b17ffbe3f376b146045ab0ac885900
Leander Hofmann <leander.hofmann@troiber.de>
2022-11-27 17:00:55 +0100

    feat: added default configuration

---
+++ lua/surrealdb-nvim/default.lua
@@ -0,0 +1,38 @@
+local M = {}
+
+local split_direction = { VERTICAL = "vertical", HORIZONTAL = "horizontal" }
+
+M.connection_mapping = {
+	host = "localhost:8000/sql",
+	ns = "test",
+	db = "test",
+	user = "root",
+	pass = "root",
+}
+
+M.connection = M.connection_mapping
+
+M.file = {
+	extensions = { "*.sdb", "*.surql", "*.sql" },
+	run_on_safe = false,
+	type = "sql",
+}
+
+M.keymaps = {
+	{ "n", "<leader>5", ":SurrealDBRun buf<CR>" },
+	{ "n", "<leader>5l", ":SurrealDBRun ln<CR>" },
+	{ "v", "<leader>5", ":<C-U>SurrealDBRun sel<CR>" },
+}
+
+M.virtual_texts = {
+	success = { "", "GreenSign" },
+	fail = { "", "RedSign" },
+}
+
+M.output = {
+	buf_name = "SurrealDB Response",
+	split = split_direction.VERTICAL,
+	format_cmd = ":%!jq --indent 2",
+}
+
+return M
