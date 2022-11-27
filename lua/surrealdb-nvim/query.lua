5ba3b321673df58659fbbfed70e75a6462e06bf8
Leander Hofmann <leander.hofmann@troiber.de>
2022-11-27 17:15:46 +0100

    feat: added methods to execute a query

--- lua/surrealdb-nvim/query.lua
+++ lua/surrealdb-nvim/query.lua
@@ -24,4 +24,58 @@ local function compose_request(query, connection)
 		connection.host
 	)
 end
+
+---executes the passed command
+---@param command string
+---@return table
+local function fetch(command)
+	local json = {}
+	local stderr = ""
+	local job_id = fn.jobstart(command, {
+		stdout_buffered = true,
+		stderr_buffered = true,
+		on_stdout = function(_, data, _)
+			table.insert(json, fn.join(data))
+		end,
+		on_stderr = function(_, data, _)
+			stderr = stderr .. fn.join(data)
+		end,
+	})
+
+	fn.jobwait({ job_id })
+	return json
+end
+
+---executes the passed query on the passed connection.
+---@param query string
+---@param connection table|nil
+local function execute(query, connection)
+	local call_fn = compose_request(query, connection)
+	local output = fetch(call_fn)
+	display.buffer_output(output)
+end
+
+---executes the content of the passed buffer.
+---@param bufnr? integer
+function M.execute_buffer(bufnr)
+	local buffer_content = utils.get_buffer_content(bufnr)
+	execute(buffer_content, nil)
+end
+
+---executes the line under the cursor.
+function M.execute_line()
+	local line = api.nvim_get_current_line()
+	execute(line, nil)
+	display.highlight_line(api.nvim_win_get_cursor(0)[1])
+end
+
+---executes the visual selection from the passed buffer.
+---@param bufnr? integer
+function M.execute_selection(bufnr)
+	local text = utils.get_visual_selection(bufnr)
+	local sr, sc, er, ec = utils.get_visual_selection_range()
+	execute(text, nil)
+	display.highlight_query(sr, sc, er, ec)
+end
+
 return M
