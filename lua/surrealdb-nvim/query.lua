20c9269bd5d610ae6f5b817f71147c1adc56f5aa
Leander Hofmann <leander.hofmann@troiber.de>
2022-11-27 17:14:15 +0100

    feat: added query base

---
+++ lua/surrealdb-nvim/query.lua
@@ -0,0 +1,9 @@
+local utils = require("lua.surrealdb-nvim.utils")
+local display = require("lua.surrealdb-nvim.display")
+local config = require("lua.surrealdb-nvim.config")
+
+local api = vim.api
+local fn = vim.fn
+local M = {}
+
+return M
