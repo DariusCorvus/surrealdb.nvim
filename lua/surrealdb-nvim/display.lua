1ff7e955e5f89361574946a6a948b44bbd25af31
Leander Hofmann <leander.hofmann@troiber.de>
2022-11-27 17:17:12 +0100

    feat: added method to initialize a new buffer

--- lua/surrealdb-nvim/display.lua
+++ lua/surrealdb-nvim/display.lua
@@ -8,4 +8,17 @@ local M = {}
 local ns_id = api.nvim_create_namespace("surrealdb-nvim")
 local ns_query_highlight_id = api.nvim_create_namespace("surrealdb-nvim-query-highlight")
 
+---initializes the current buffer for the surrealdb response.
+---@param split_cmd string vim command for the split direction
+local function init_buffer(split_cmd)
+	cmd(split_cmd)
+	cmd(":enew")
+	cmd(":setlocal buftype=nofile")
+	cmd(":setlocal bufhidden=hide")
+	cmd(":setlocal noswapfile")
+	cmd(":setlocal nonumber norelativenumber scl=no")
+	cmd(":set filetype=json")
+	cmd(":file " .. config.output.buf_name .. "")
+end
+
 return M
