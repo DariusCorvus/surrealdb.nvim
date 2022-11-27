b84f2f80c223919a0f5e783769c9794bdcb5aa64
Leander Hofmann <leander.hofmann@troiber.de>
2022-11-27 17:16:55 +0100

    feat: added locals for virtual text namespaces

--- lua/surrealdb-nvim/display.lua
+++ lua/surrealdb-nvim/display.lua
@@ -5,4 +5,7 @@ local fn = vim.fn
 local cmd = vim.cmd
 local M = {}
 
+local ns_id = api.nvim_create_namespace("surrealdb-nvim")
+local ns_query_highlight_id = api.nvim_create_namespace("surrealdb-nvim-query-highlight")
+
 return M
