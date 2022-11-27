514302e564a82cf4ded907849f4638e7f829967a
Leander Hofmann <leander.hofmann@troiber.de>
2022-11-27 17:20:50 +0100

    feat: added command base

---
+++ lua/surrealdb-nvim/command.lua
@@ -0,0 +1,9 @@
+local query = require("lua.surrealdb-nvim.query")
+local utils = require("lua.surrealdb-nvim.utils")
+local config = require("lua.surrealdb-nvim.config")
+local default = require("lua.surrealdb-nvim.default")
+local display = require("lua.surrealdb-nvim.display")
+
+local api = vim.api
+local cmd = vim.cmd
+
