d85b494a3cd7db5e4f4b98e9373dbb4cf6b2b3ca
Leander Hofmann <leander.hofmann@troiber.de>
2022-11-27 17:11:18 +0100

    feat: added helper method to update config from setup function

--- lua/surrealdb-nvim/config.lua
+++ lua/surrealdb-nvim/config.lua
@@ -3,4 +3,9 @@ local default = require("surrealdb-nvim.default")
 local fn = vim.fn
 
 local M = default
+
+function M.update_config(opts)
+	M = utils.recursive_table_update(M, opts)
+end
+
 return M
