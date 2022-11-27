dd86595cc824f8b19a7b2e7639ff06ad94b534f6
Leander Hofmann <leander.hofmann@troiber.de>
2022-11-27 17:14:45 +0100

    feat: added method to compose cmd from query string and connection table

--- lua/surrealdb-nvim/query.lua
+++ lua/surrealdb-nvim/query.lua
@@ -6,4 +6,22 @@ local api = vim.api
 local fn = vim.fn
 local M = {}
 
+---returns the composed command with the passed query and connection.
+---@param query string
+---@param connection table|nil
+---@return string
+local function compose_request(query, connection)
+	if connection == nil then
+		connection = config.connection
+	end
+	return string.format(
+		'curl -u %s:%s -d "%s" --header "Accept: application/json" --header "NS: %s" --header "DB: %s" %s',
+		connection.user,
+		connection.pass,
+		query,
+		connection.ns,
+		connection.db,
+		connection.host
+	)
+end
 return M
