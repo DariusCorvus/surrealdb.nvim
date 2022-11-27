455ea38f3b9cf134348cfff1177636129ce7e2f2
Leander Hofmann <leander.hofmann@troiber.de>
2022-11-27 17:02:38 +0100

    feat: added nested table update for setup method

--- lua/surrealdb-nvim/utils.lua
+++ lua/surrealdb-nvim/utils.lua
@@ -1,3 +1,17 @@
+---recursivly updates the passed destination table with the source table, over every depth of nested tables.
+---@param dest table
+---@param src table
+---@return table
+function M.recursive_table_update(dest, src)
+	for key, value in pairs(src) do
+		if type(value) == "table" then
+			dest[key] = M.recursive_table_update(dest, value)
+		else
+			dest[key] = value
+		end
+	end
+	return dest
+end
 ---splits a string into a table through a passed pattern.
 ---@param str string
 ---@param pattern string
